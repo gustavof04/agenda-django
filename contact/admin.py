4ba8d2ddf847bb0e24d7f8ab4253f054912c5460
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-09-18 15:39:44 -0400

    feat: Adiciona field estrangeiro category e configura no admin

--- contact/admin.py
+++ contact/admin.py
@@ -8,3 +8,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
