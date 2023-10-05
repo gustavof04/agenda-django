637b8053f94789e1bc9c0f598af6f4b766c32db8
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-10-05 10:17:52 -0400

    chore: Usa request.method e .POST para saber quando o form é postado

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -6,9 +6,20 @@ from contact.models import Contact
 
 # Create your views here.
 def create(request):
+    if request.method == 'POST':
+            print()
+            print(request.method)
+            print(request.POST.get('first_name'))
+            print(request.POST.get('last_name'))
+            print()
+
     context = {
 
     }
+
+    print()
+    print(request.method)
+    print()
 
     return render(
         request,
