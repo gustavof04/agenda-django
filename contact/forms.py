8ad15ceb41b1098de3bea64bdf7a16c3c1e9ac54
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-10-06 14:43:53 -0400

    chore: Adiciona validação de nomes iguais no form de Usuário

--- contact/forms.py
+++ contact/forms.py
@@ -70,6 +70,21 @@ class RegisterForm(UserCreationForm):
             'username', 'password1', 'password2',
         )
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
 
