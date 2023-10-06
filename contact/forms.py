c86364544b9a6a4c37355cb183c02328c1caf87e
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-10-06 13:16:29 -0400

    feat: Adiciona campo de atualizar imagem no form exibindo ela

--- contact/forms.py
+++ contact/forms.py
@@ -4,25 +4,20 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-                'class': 'class-a class-b',
-                'placeholder': 'Aqui veio do init',
+                'accept': 'image/*',
             }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário'
+        )
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
         )
 
     def clean(self):
